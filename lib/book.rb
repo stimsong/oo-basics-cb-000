a0fcbf296f3a49753ae72983ae0e7514694f332f
Greg Stimson <stimsong@yahoo.com>
2018-03-24 22:34:33 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -12,23 +12,23 @@ class Book
   # def author=(author)
   #   @author = author
   # end
-  # 
+  #
   # def author
   #   @author
   # end
-  # 
+  #
   # def page_count=(num)
   #   @page_count = num
   # end
-  # 
+  #
   # def page_count
   #   @page_count
   # end
-  # 
+  #
   # def genre=(genre)
   #   @genre = genre
   # end
-  # 
+  #
   # def genre
   #   @genre
   # end
