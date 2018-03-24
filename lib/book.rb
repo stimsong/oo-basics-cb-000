a6c0bad528dd8626eb21f75226307675e8d06997
Greg Stimson <stimsong@yahoo.com>
2018-03-24 22:39:16 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -11,7 +11,7 @@ class Book
   # def title
   #   @title
   # end
-  # 
+  #
   # def author=(author)
   #   @author = author
   # end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,5 @@
 # Make your shoe class here!
+
+def Shoe(brand)
+
+end
