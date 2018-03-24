da0d61711725a308ced3d978d01742d2052d288d
Greg Stimson <stimsong@yahoo.com>
2018-03-24 23:00:12 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,22 +1,23 @@
 # Make your shoe class here!
 
 class Shoe
-  attr_accessor :brand, :color, :size, :material
+  attr_accessor :brand, :color, :size, :material, :condition
 #  attr_reader :condition
 
   def initialize(brand)
     @brand = brand
   end
 
-  def condition=(condition)
-    @condition = condition
-  end
-
-  def condition
-    @condition
-  end
+  # def condition=(condition)
+  #   @condition = condition
+  # end
+  # 
+  # def condition
+  #   @condition
+  # end
 
   def cobble
+    @condition = "new"
     puts "Your shoe is as good as new!"
   end
 
